6f1d90196f6dc6a922f8db75d487071dd88309b9
ibrahim-ahmed201 <ibrahimahmed384888@gmail.com>
2025-06-24 23:44:03 +0300

    Handling The Routes And Css Modules

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import HomePage from "./pages/HomePage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
-
+import "./index.css";
+import Login from "./pages/Login";
+import AppLayout from "./pages/AppLayout";
 export default function App() {
   return (
     <BrowserRouter>
@@ -11,6 +13,8 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="App" element={<AppLayout />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,10 @@
+import PageNav from "../components/PageNav";
+
+export default function AppLayout() {
+  return (
+    <div>
+      <PageNav />
+      <h1>AppLayout component</h1>
+    </div>
+  );
+}
